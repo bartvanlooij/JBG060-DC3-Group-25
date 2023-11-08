0ab27444ae55a16e6e5c951f248976569e1e0e4d
aeissa2003 <39440812+aeissa2003@users.noreply.github.com>
2023-11-08 05:24:52 +0100

    Add files via upload

---
+++ bias_of_neighboring_countires.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+
+data = pd.read_csv('full_dataset_with_sentiment_combined2.csv')
+
+world = gpd.read_file('countries.geojson')
+
+def get_country(lat, lng):
+    point = Point(lng, lat)
+    country = world[world.geometry.contains(point)]['ADMIN'].values
+    return country[0] if country.size > 0 else None
+
+
+data['country'] = data.apply(lambda row: get_country(row['lat'], row['lng']), axis=1)
+
+# calculate the proportion of positive sentiments to negative ones per neighboring country
+data['positive_sentiment'] = data['sentiment_paragraph'] == 'POSITIVE'
+positive_proportion = data.groupby('country')['positive_sentiment'].mean()
+
+# neighboring countries
+target_countries = [
+    'Ethiopia', 'Sudan', 'Central African Republic', 
+    'Democratic Republic of the Congo', 'Uganda', 'Kenya'
+]
+
+
+bias_per_country = positive_proportion.loc[target_countries]
+
+print(bias_per_country)
